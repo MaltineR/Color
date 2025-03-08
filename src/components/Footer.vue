6f42d6ce822f3100602bc2134f9fbea2adc34142
Maltine <maltina.rama@gmail.com>
2025-03-09 00:40:39 +0100

    Titles

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,9 +2,9 @@
   <div class="bg-indigo-200 bg-opacity-25 p-6 text-xs">
     <div class="text-gray-700">
       <ul>
-        <li>Biancoir {{ year }}</li>
+        <li>UI Colors {{ year }}</li>
         <li>
-          Made with ❤️ by <a href="https://github.com/iqbaladinur" class="footer-link">Iqbal Adi</a> |
+          Made with ❤️ by <a href="https://github.com/MaltineR/Color" class="footer-link">Maltine Rama</a> |
           <a href="https://www.privacypolicygenerator.info/live.php?token=ipuHipcuvTXh4fCJtSXLkMPZrRJsT6ib" class="footer-link">Privacy Police</a> |
           <a href="https://www.termsofusegenerator.net/live.php?token=AnPvYI8rmFEQX8uG1lWqANrqpIzOw3hp" class="footer-link">Terms of Use</a> |
           Images sources from <a href="https://unsplash.com/" class="footer-link">unsplash.com</a>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,12 +3,12 @@
     <div class="container mx-auto flex items-center justify-between flex-wrap">
       <div>
         <span class="color-grad logo text-2xl">
-          Biancoir
+          UI Colors
         </span>
       </div>
       <div class="flex-grow flex flex-row-reverse lg:items-center w-auto">
         <div>
-          <a href="https://github.com/iqbaladinur/color-extractor"
+          <a href="https://github.com/MaltineR/Color"
              class="inline-block text-sm p-2 leading-none  rounded text-white hover:border-transparent hover:text-white"
           >
             <img src="@/assets/github.svg" width="20" height="20" class="inline text-white">
